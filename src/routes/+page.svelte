d17c1243e544378d7e964050cdcc371ba7e2aee3
Calvin Wong <ctwong958@berkeley.edu>
2024-02-04 09:19:18 -0800

    Smithing in svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import * as THREE from 'three';
+	import * as SC from 'svelte-cubed';
+  const tip_width = 0.635 /2;
+  const longsword_blade_length = 10.0;
+</script>
+
+<SC.Canvas
+	antialias
+	fog={new THREE.FogExp2('papayawhip', 0.01)}
+	background={new THREE.Color('papayawhip')}
+	shadows
+>
+	<!-- <SC.Group position={[0, -1 / 2, 0]}>
+		<SC.Mesh
+			geometry={new THREE.PlaneGeometry(50, 50)}
+			material={new THREE.MeshStandardMaterial({ color: 'burlywood' })}
+			rotation={[-Math.PI / 2, 0, 0]}
+			receiveShadow
+		/>
+
+		<SC.Primitive
+			object={new THREE.GridHelper(50, 50, 0x444444, 0x555555)}
+			position={[0, 0.001, 0]}
+		/>
+	</SC.Group> -->
+
+  <SC.Group position={[0, 5, 0]}>
+    <SC.Mesh
+      geometry={new THREE.CylinderGeometry(tip_width, tip_width, longsword_blade_length)}
+      material={new THREE.MeshStandardMaterial({ color: 0xff3e00 })}
+    />
+  </SC.Group>
+	
+	<SC.PerspectiveCamera position={[1, 1, 3]} />
+	<SC.OrbitControls enableZoom={true} /> 
+
+	<SC.AmbientLight intensity={0.6} />
+	<SC.DirectionalLight intensity={0.6} position={[-2, 3, 2]} shadow={{ mapSize: [2048, 2048] }} />
+	​</SC.Canvas
+>
